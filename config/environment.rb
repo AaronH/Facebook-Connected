5a081683ab316cb47ab39ca4a4bc79e69eac2e5d
Aaron Hurley <aaron@aaronhurley.com>
2010-01-14 12:40:46 -0800

    Add factory_girl and shoulda. Moved to haml-edge for color manipulation

--- config/environment.rb
+++ config/environment.rb
@@ -19,8 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml"
-  config.gem "rubyist-aasm", :source => "http://gems.github.com", :lib => 'aasm'
+  config.gem "haml-edge"
+  config.gem 'workflow'
   config.gem "facebooker"
   
 
